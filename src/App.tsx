d2cde117672ab0454ec5ec1c23e0d76e5ecb9d69
GODZISOP <appointmentstudio@gmail.com>
2025-07-12 13:59:27 +0500

    push

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
 
